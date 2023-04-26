931dc97df5fcb0f9beac1adefc6a65c5d6bf0181
Samuel Barbosa <samuca192silva@hotmail.com>
2023-04-26 13:53:38 -0300

    feat - Install packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="primary" size="lg">
+          Large button
+        </Button>
       </header>
     </div>
   );
