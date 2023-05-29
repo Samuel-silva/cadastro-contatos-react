f469791e774542a95752384cb07dffd3f65132d9
Samuel Barbosa <samuca192silva@hotmail.com>
2023-05-29 18:54:53 -0300

    feat - Github pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import PageNotFound from "../pages/PageNotFound";
 import FormPage from "../pages/FormPage";
+import { Navigate } from 'react-router-dom';
 
 export default function MainRoutes() {
 	return (
@@ -11,7 +12,8 @@ export default function MainRoutes() {
 			<Route path="/" element={<Home />} />
 			<Route path="/new-register" element={<FormPage />} />
 			<Route path="/edit/:id" element={<FormPage />} />
-			<Route path="*" element={<PageNotFound />} />
+			<Route path='/404' element={<PageNotFound/>} />
+			<Route path='*' element={<Navigate replace to='/404'/>} />
 		</Routes>
 	);
 }
