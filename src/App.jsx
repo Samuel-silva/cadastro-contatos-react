20e53b44afce9f4beb50ca9dc03bfc9ff96d8149
Samuel Barbosa <samuca192silva@hotmail.com>
2023-11-13 22:05:32 -0300

    refactor - React DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { BrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
 
-import Routes from "./routes";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import FormPage from "./pages/FormPage";
+
+// import Routes from "./routes";
 import Store from "./Store";
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />}>
+      <Route index element={<Home />} />
+      <Route path="new-register" element={<FormPage />} />
+      <Route path="edit/:id" element={<FormPage />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Route>
+  )
+)
 function App() {
   return (
     <Store>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Store>
   );
 }
